d3f2912f0582b955cae43f7abbce0d09c2d7efd0
BED-COM-32-20 <kelvinmponda47@gmail.com>
2025-08-07 17:41:11 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'screens/splash_screen.dart';
+import 'screens/onboarding_screen.dart';
+import 'screens/auth/login_screen.dart';
+import 'screens/auth/register_screen.dart';
+import 'screens/home/dashboard_screen.dart';
+import 'screens/diagnosis/camera_screen.dart';
+import 'screens/diagnosis/result_screen.dart';
+
+void main() {
+  runApp(ScabiesDiagnosisApp());
+}
+
+class ScabiesDiagnosisApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Scabies Diagnosis',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: Color(0xFF2196F3),
+        scaffoldBackgroundColor: Colors.white,
+        fontFamily: 'Roboto',
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => SplashScreen(),
+        '/onboarding': (context) => OnboardingScreen(),
+        '/login': (context) => LoginScreen(),
+        '/register': (context) => RegisterScreen(),
+        '/dashboard': (context) => DashboardScreen(),
+        '/camera': (context) => CameraScreen(),
+        '/result': (context) => ResultScreen(),
+      },
+    );
+  }
+}
